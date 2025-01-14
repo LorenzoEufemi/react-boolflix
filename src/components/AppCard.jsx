bb700aab12627e3f6a3b1455e78746e7d1b7e4d3
LorenzoEufemi <lorenzoeufemi@gmail.com>
2025-01-14 18:38:02 +0100

    feat: consegna

---
+++ src/components/AppCard.jsx
@@ -0,0 +1,15 @@
+function AppCard() {
+
+
+    return(
+        <div>
+            <p>Titolo:</p>
+            <p>Titolo originale:</p>
+            <p>Voto:</p>
+            <p>Overview:</p>
+            
+
+        </div>
+
+    )
+}
